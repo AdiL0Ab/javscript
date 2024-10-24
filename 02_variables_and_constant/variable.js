ca873aed6eed0b228135b19ee585a4cbae2b7597
AdiL0Ab <adirbbeabhi2001@gmail.com>
2024-10-24 05:47:50 +0000

    variable

---
+++ 02_variables_and_constant/variable.js
@@ -0,0 +1,17 @@
+let a =2;
+a=6;
+var a1 = 5;
+const b = 3;
+console.log("let is "+a);
+console.log("Var is "+a1);
+
+{
+    //b=5;
+    console.log("const is "+b);
+   let a=4;
+    console.log("let is "+a);
+    var a1 = 8;
+    console.log("var is "+a1);
+}
+console.log("var is "+a1);
+console.log("let is "+a);
