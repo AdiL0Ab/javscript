8eb78ef44db3b37103074a78cf39940adaf8bb85
AdiL0Ab <adirbbeabhi2001@gmail.com>
2024-10-24 05:52:36 +0000

    Creating table

--- 02_variables_and_constant/variable.js
+++ 02_variables_and_constant/variable.js
@@ -2,6 +2,8 @@ let a =2;
 a=6;
 var a1 = 5;
 const b = 3;
+city = "Darbhanga";
+let ghar;
 console.log("let is "+a);
 console.log("Var is "+a1);
 
@@ -15,3 +17,4 @@ console.log("Var is "+a1);
 }
 console.log("var is "+a1);
 console.log("let is "+a);
+console.table([a,a1,b,city,ghar]);
