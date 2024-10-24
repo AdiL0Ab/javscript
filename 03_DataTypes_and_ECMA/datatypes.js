4985c0905d5b51334b468d4748cd85e577bc8905
AdiL0Ab <adirbbeabhi2001@gmail.com>
2024-10-24 07:24:26 +0000

    datatypes

---
+++ 03_DataTypes_and_ECMA/datatypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all code as newer version
+
+//alert(3+3); // giving pop up on website
+
+//we use mdn or c39.es (original document) to get the documentation 
+
+let num = 45;
+let num2 = 45.55
+let a = "Abhishek"
+let ab = true;
+let abc = null;
+let bcd ;
+
+/*
+ Datatypes :-
+  1. Number => 2 to power 53 // it supports both integer and float value 
+  2. bigint => The range of BigInt is not fixed but limited by the available memory of the system. 
+  In theory, it can store arbitrarily large integers, both positive and negative. 
+  There is no defined upper or lower bound on BigInt values, unlike Number values,
+   which are constrained by floating-point precision.
+  3. string => ""
+  4. boolean => true/false 
+  5. null => standalone value
+  6. undefined => value is not defined
+  7. symbol => when we talk about unique 
+
+  8. object 
+*/
+console.log(typeof(a)+" ", typeof(num)+" ", typeof(num2)+" ", typeof(ab)+" ", typeof(abc)+" ", typeof(bcd));
+
