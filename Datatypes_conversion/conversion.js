1f6ef93a9d36e74985dc4285a6327e16bc4af8ce
AdiL0Ab <adirbbeabhi2001@gmail.com>
2024-10-24 08:58:05 +0000

    typcast

---
+++ Datatypes_conversion/conversion.js
@@ -0,0 +1,7 @@
+let score = 12;
+let marks ="45.5abc"
+let valueInstring = String(score);
+console.log("Score is "+typeof(valueInstring));
+let valueInNumer = Number(marks)
+console.log("Marks is "+typeof(valueInNumer));
+console.log(valueInNumer)
